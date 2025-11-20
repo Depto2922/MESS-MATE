1d0f07883f83c1b00427443e0f5d008a87b5b692
Depto2922 <vk640482@gmail.com>
2025-11-20 20:23:18 +0600

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+// Firebase Web App Config
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyDen1i84TSL2lJ_7C4U0ojz3iBnNuQHhBU",
+  authDomain: "mess-mate-203c4.firebaseapp.com",
+  projectId: "mess-mate-203c4",
+  storageBucket: "mess-mate-203c4.firebasestorage.app",
+  messagingSenderId: "1022805754428",
+  appId: "1:1022805754428:web:839dc3bf40b76cf4327939"
+};
